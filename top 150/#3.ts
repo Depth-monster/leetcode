cd36fce80b26e1b5562f9a76553ac32912f66339
Azat Hajyyev <122405130+Depth-monster@users.noreply.github.com>
2023-03-24 01:00:17 +0300

    Update #3.ts

--- top 150/#3.ts
+++ top 150/#3.ts
@@ -1,1 +1,26 @@
 console.log('3rd leetcode')
+function lengthOfLongestSubstring(s: string): number {
+    const substrings = s.split('');
+    let longestSubstringLength = 0;
+
+    for (let i = 0; i < substrings.length; i++) {
+        let seenChars = new Set();
+        let currentSubstringLength = 0;
+
+        for (let j = i; j < substrings.length; j++) {
+            if (!seenChars.has(substrings[j])) {
+                seenChars.add(substrings[j]);
+                currentSubstringLength++;
+                if (currentSubstringLength > longestSubstringLength) {
+                    longestSubstringLength = currentSubstringLength;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+console.log(longestSubstringLength);
+    return longestSubstringLength;
+}
+//const s:string="abcabcdeabcd";
+//lengthOfLongestSubstring(s);
